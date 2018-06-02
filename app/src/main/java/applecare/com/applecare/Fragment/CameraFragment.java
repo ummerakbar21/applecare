78d08eac0cc5925b830ad048bc6dde6971def5a8
shabir ahmad mir <mirshabir150@gmail.com>
2018-06-02 21:18:23 +0530

    changes in design for login and signup and images saved in folder

--- app/src/main/java/applecare/com/applecare/Fragment/CameraFragment.java
+++ app/src/main/java/applecare/com/applecare/Fragment/CameraFragment.java
@@ -2,7 +2,9 @@ package applecare.com.applecare.Fragment;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -11,6 +13,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import applecare.com.applecare.R;
 
@@ -24,6 +30,7 @@ public class CameraFragment extends Fragment {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private ImageView caputuredImageView;
     private ImageButton cameraImageButton;
+    Uri uriSavedImage;
     public CameraFragment(){}
 
     @Override
@@ -37,18 +44,31 @@ public class CameraFragment extends Fragment {
     }
 
     private void dispatchTakePictureIntent(View view) {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        Intent pictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+
+//folder stuff
+        File imagesFolder = new File(Environment.getExternalStorageDirectory(), "MyImages");
+        imagesFolder.mkdirs();
+
+        File image = new File(imagesFolder, "QR_" + timeStamp + ".png");
+        uriSavedImage = Uri.fromFile(image);
+
+        pictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, uriSavedImage);
+        if (pictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(pictureIntent, REQUEST_IMAGE_CAPTURE);
         }
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            caputuredImageView.setImageBitmap(imageBitmap);
+           /* Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");*/
+            File imgFile = new  File(uriSavedImage.getPath());
+            if(imgFile.exists())            {
+                caputuredImageView.setImageURI(Uri.fromFile(imgFile));
+            }
         }
     }
 
