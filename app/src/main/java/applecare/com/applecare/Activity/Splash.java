d877be9d8031c477f96805e0e959c6eac91c2cd5
ummerakbar45 <ummerakbar21@gmail.com>
2020-11-21 11:09:47 +0530

    added UI for login

--- app/src/main/java/applecare/com/applecare/Activity/Splash.java
+++ app/src/main/java/applecare/com/applecare/Activity/Splash.java
@@ -3,7 +3,9 @@ package applecare.com.applecare.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
 import applecare.com.applecare.R;
@@ -13,6 +15,14 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity( new Intent(Splash.this, LoginActivity.class));
+
+            }
+        },3000);
     }
 
     @Override
@@ -22,6 +32,7 @@ public class Splash extends AppCompatActivity {
 // Hide the status bar.
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+
 // Remember that you should never show the action bar if the
 // status bar is hidden, so hide that too if necessary
     }
