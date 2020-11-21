60c81bb76ba33fe8f90e5d4015af2c0bc806d62b
Wahdat Jan <Wahdat.jan@trialx.com>
2020-11-21 12:41:24 +0530

    Sign model added

---
+++ app/src/main/java/applecare/com/applecare/Model/SignUpUser.java
@@ -0,0 +1,47 @@
+package applecare.com.applecare.Model;
+
+public class SignUpUser {
+    private String email;
+    private String name;
+    private String password;
+    private String district;
+
+    public SignUpUser(String email, String name, String password, String district) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+        this.district = district;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+}
