85caa7f8ee2b1f376a3f3b6cb594eb42f488999a
ummerakbar45 <ummerakbar21@gmail.com>
2020-11-27 22:06:56 +0530

    added main activity

--- app/src/main/java/applecare/com/applecare/Activity/LoginActivity.java
+++ app/src/main/java/applecare/com/applecare/Activity/LoginActivity.java
@@ -95,8 +95,8 @@ public class LoginActivity extends AppCompatActivity {
 
                 if (task.isSuccessful()){
                     waitingDialog.dismiss();
-//                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
-//                    startActivity(intent);
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                   startActivity(intent);
                  //   finish();
                 }
 
