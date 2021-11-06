09d67478efd5e1cbe4f93b8d6d9a6f424bf0c6f2
Kundroomajid <kundroomajid@gmail.com>
2021-11-06 23:15:06 +0530

    adds user allready registered msg in UI and added error handling classes for that

--- app/src/main/java/applecare/com/applecare/Utils/ErrorUtils.java
+++ app/src/main/java/applecare/com/applecare/Utils/ErrorUtils.java
@@ -1,2 +1,29 @@
-package applecare.com.applecare.Utils;public class ErrorUtils {
+package applecare.com.applecare.Utils;
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+
+import applecare.com.applecare.Model.ErrorResponse;
+import okhttp3.ResponseBody;
+import retrofit2.Converter;
+import retrofit2.Response;
+
+import static applecare.com.applecare.network.APIClient.getClient;
+
+public class ErrorUtils {
+    public static ErrorResponse parseError(Response<?> response) {
+        Converter<ResponseBody, ErrorResponse> converter =
+                getClient()
+                        .responseBodyConverter(ErrorResponse.class, new Annotation[0]);
+
+        ErrorResponse error;
+
+        try {
+            error = converter.convert(response.errorBody());
+        } catch (IOException e) {
+            return new ErrorResponse();
+        }
+
+        return error;
+    }
 }
